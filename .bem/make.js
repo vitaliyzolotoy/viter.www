dd3462dc382be7a32c29d6488f0b4bc9a7e3560e
Vitaly Zolotoy <vizo@yandex-team.ru>
2012-09-05 20:14:41 +0300

    оновив вітку бібліотеки bem-bl

--- .bem/make.js
+++ .bem/make.js
@@ -6,7 +6,7 @@ MAKE.decl('Arch', {
             'bem-bl': {
                 type: 'git',
                 url: 'git://github.com/bem/bem-bl.git',
-                treeish: 'master'
+                treeish: '0.3'
             }
         };
 
