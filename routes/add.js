d68265fd1ebff8f75c03786eb0ec727c8b6f9d6c
Vitaly Zolotoy <vizo@yandex-team.ru>
2012-11-10 19:36:56 +0200

    post submit page

---
+++ routes/add.js
@@ -0,0 +1,20 @@
+exports.index = function (req, res, Page, fs, path, vm) {
+    var priv,
+        bemjson,
+        bemhtml;
+
+    Page.findOne({}, function (err, data) {
+        if(err);
+
+        fs.readFile(path.join(__dirname, '..', 'pages-desktop', 'add', '_add.priv.js'), 'utf-8', function(err, file){
+            if(err);
+
+            vm.runInThisContext(file);
+
+            bemjson = blocks['b-page']({ page: data});
+            bemhtml = BEMHTML.apply(bemjson);
+
+            res.send(bemhtml);
+        });
+    })
+};
