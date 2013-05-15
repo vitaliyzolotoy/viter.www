4f5bb837074b0c95639b98e76efce00cdf38965a
Vitaly Zolotoy <vizo@yandex-team.ru>
2013-05-15 23:06:27 +0300

    remove api page

--- viter.js
+++ viter.js
@@ -2,12 +2,9 @@
 
 var express = require('express'),
     http = require('http'),
-    viter = express();
+    viter = express(),
 
-    ctrl1 = require('./desktop.bundles/index/_index.ctrl.js'),
-    ctrl2 = require('./desktop.bundles/meanwhile/_meanwhile.ctrl.js'),
-    ctrl3 = require('./desktop.bundles/article/_article.ctrl.js'),
-    ctrl4 = require('./desktop.bundles/create/_create.ctrl.js');
+    ctrl2 = require('./desktop.bundles/meanwhile/_meanwhile.ctrl.js');
 
 viter.configure(function(){
     viter.set('port', process.env.PORT || 8787);
@@ -23,24 +20,8 @@ viter.configure('development', function(){
   viter.use(express.errorHandler());
 });
 
-viter.get('/', function(request, response) {
-    ctrl1.index(request, response);
-});
-
 viter.get('/meanwhile/', function(request, response) {
     ctrl2.meanwhile(request, response);
-});
-
-viter.get('/article/', function(request, response) {
-    ctrl3.article(request, response);
-});
-
-viter.get('/create/', function(request, response) {
-    ctrl4.create(request, response);
-});
-
-viter.post('/create/', function(request, response) {
-    ctrl4.create(request, response);
 });
 
 http.createServer(viter).listen(viter.get('port'), function(){
