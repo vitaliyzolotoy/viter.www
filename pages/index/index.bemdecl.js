99a541f336b813b06ea15ef440de0f3ddbf779c3
Vitaly Zolotoy <vitaliyzolotoy@gmail.com>
2013-07-04 01:37:57 +0300

    add bemhtml dep

--- pages/index/index.bemdecl.js
+++ pages/index/index.bemdecl.js
@@ -3,6 +3,7 @@ exports.blocks = [
     // bem-node part
 
     {block: 'i-router', mods: {init: 'auto'}},
+    {block: 'i-content', mods: {type: 'bemhtml'}},
     {block: 'i-ycssjs'},
 
     // user part
