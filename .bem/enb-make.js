4eef1417e840514ed05e99f3fe94cb8a74caf594
Vitaly Zolotoy <vitaliyzolotoy@gmail.com>
2013-07-14 21:46:35 +0300

    make

--- .bem/enb-make.js
+++ .bem/enb-make.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
     config.mode("production", function() {
         config.node("pages/index", function(nodeConfig) {
             nodeConfig.addTechs([
-                [ require("enb/techs/borschik"), { sourceTarget: "?.js", destTarget: "_?.js", minify: true, freeze: true } ],
+                [ require("enb/techs/borschik"), { sourceTarget: "?.js", destTarget: "_?.js", minify: true, freeze: false } ],
                 [ require("enb/techs/borschik"), { sourceTarget: "?.css", destTarget: "_?.css", minify: true, freeze: true } ]
             ]);
         });
@@ -22,13 +22,13 @@ module.exports = function(config) {
             [ require("enb/techs/file-provider"), { target: "?.bemdecl.js" } ],
             require("enb/techs/deps-old"),
             require("enb/techs/files"),
-            require("enb/techs/js"),
+            // require("enb/techs/js"),
             require("enb/techs/css"),
 
             require("enb-bemhtml/techs/bemhtml"),
             require("../.bem/techs/server"),
             require("../.bem/techs/priv"),
-            // require("../.bem/techs/js")
+            require("../.bem/techs/js")
         ]);
         nodeConfig.addTargets([
             "_?.js",
