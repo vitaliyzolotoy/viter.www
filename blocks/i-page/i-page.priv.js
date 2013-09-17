de6a90e3f95c8dfb6419d23d2ce1301ac7d8fc78
Vitaly Zolotoy <vitaliyzolotoy@gmail.com>
2013-09-17 13:48:53 +0300

    update bem-node module

--- blocks/i-page/i-page.priv.js
+++ blocks/i-page/i-page.priv.js
@@ -1,54 +1,52 @@
 BEM.decl('i-page', null, {
     getJson: function(json) {
-        return [
-            {
-                block: 'b-page',
-                title: 'The Feature',
-                head: [
-                    { elem: 'favicon', url: '/favicon.ico' }
-                ],
-                content: [
-                    {
-                        block: 'header',
-                        content: [
-                            {
-                                block: 'logo',
-                                content: [
-                                    {
-                                        block: 'link',
-                                        url: '/',
-                                        content: 'The Feature'
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        block: 'content',
-                        content: json
-                    },
-                    {
-                        block: 'footer',
-                        content: [
-                            {
-                                block: 'copyright',
-                                content: [
-                                    '© Журнал «',
-                                    {
-                                        block: 'link',
-                                        url: '/',
-                                        content: 'The Feature'
-                                    },
-                                    '», 2013 рік'
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        block: 'analytics'
-                    }
-                ]
-            }
-        ]
+        return {
+            block: 'b-page',
+            title: 'The Feature',
+            head: [
+                { elem: 'favicon', url: '/favicon.ico' }
+            ],
+            content: [
+                {
+                    block: 'header',
+                    content: [
+                        {
+                            block: 'logo',
+                            content: [
+                                {
+                                    block: 'link',
+                                    url: '/',
+                                    content: 'The Feature'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    block: 'content',
+                    content: json
+                },
+                {
+                    block: 'footer',
+                    content: [
+                        {
+                            block: 'copyright',
+                            content: [
+                                '© Журнал «',
+                                {
+                                    block: 'link',
+                                    url: '/',
+                                    content: 'The Feature'
+                                },
+                                '», 2013 рік'
+                            ]
+                        }
+                    ]
+                },
+                {
+                    block: 'analytics'
+                }
+            ]
+        }
     }
 });
